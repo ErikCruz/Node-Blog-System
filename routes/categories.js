d298710615c204b39c075f6a3bbbdb03f2fda0d5
Erik Cruz <erikc1989@live.com>
2015-07-08 04:06:13 +0000

    Implement adding a category

---
+++ routes/categories.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+
+router.get('/add', function(req, res, next){
+    res.render('addcategory', {'title': 'Add Category'});
+});
+
+router.post('/add', function(req, res, next){
+    var title = req.body.title;
+    
+    req.checkBody('title', 'Title field is required').notEmpty();
+    var errors = req.validationErrors();
+    
+    if(errors) {
+        res.render('addcategory', {
+            'title': title,
+            'errors': errors
+        });
+    } else {
+        var categories = db.get('categories');
+        categories.insert({'title': title}, function(err, category){
+            if(err) {
+                res.send('There was an issue submitting the category');
+            }
+            req.flash('success', 'Category Submitted');
+            res.location('/');
+            res.redirect('/');
+        });
+    }
+});
+
+module.exports = router;
